ce2531091151c38da6ab23450cce2f033d79ef39
Chris Wilson <hey@chris-wilson.me>
2024-03-08 14:37:13 +0000

    Fix SNAs in certificate
    The trailing comma in the list of instance IPs was causing an empty DNS:
    entry to be added to the certificate, which is invalid syntax and caused
    authentication to fail.

--- scripts/generate-kubernetes-certs.sh
+++ scripts/generate-kubernetes-certs.sh
@@ -7,7 +7,7 @@ TLS_PATH=tls
 INSTANCE_IPS=$(
   aws --profile kthw ec2 describe-instances \
     --query 'Reservations[*].Instances[*].[PublicIpAddress,PrivateIpAddress]' \
-    --output text | tr '\t' ',' | tr '\n' ','
+    --output text | tr '\t' ',' | tr '\n' ',' | sed 's/,$//g'
 )
 
 KUBERNETES_HOSTNAMES=kubernetes,kubernetes.default,kubernetes.default.svc,kubernetes.default.svc.cluster,kubernetes.svc.cluster.local
