08c5049f5e5729ccef27b77dc32e3ece7fd99524
Chris Wilson <hey@chris-wilson.me>
2024-03-11 02:11:26 +0000

    Create security group rule resources

---
+++ aws/aws_security_group_rules.tf
@@ -0,0 +1,10 @@
+resource "aws_security_group_rule" "this" {
+  for_each          = { for index, rule in local.security_group_rules : index => rule }
+  type              = each.value.type
+  from_port         = each.value.from_port
+  to_port           = each.value.to_port
+  protocol          = each.value.protocol
+  cidr_blocks       = each.value.cidr_blocks
+  description       = each.value.description
+  security_group_id = aws_security_group.instance.id
+}
--- aws/locals.tf
+++ aws/locals.tf
@@ -13,4 +13,47 @@ locals {
     { name = "worker-2", private_ip = "10.240.1.5", pod_cidr = "10.200.1.0/24", },
     { name = "worker-3", private_ip = "10.240.2.5", pod_cidr = "10.200.2.0/24", },
   ]
+
+  security_group_rules = [
+    {
+      from_port   = 6443,
+      to_port     = 6443,
+      protocol    = "tcp",
+      cidr_blocks = ["0.0.0.0/0"],
+      type        = "ingress",
+      description = "Allow traffic to the cluster endpoint from all sources."
+    },
+    {
+      from_port   = 22,
+      to_port     = 22,
+      protocol    = "tcp",
+      cidr_blocks = ["0.0.0.0/0"],
+      type        = "ingress",
+      description = "Allow SSH traffic from all sources."
+    },
+    {
+      from_port   = 0,
+      to_port     = 0,
+      protocol    = "tcp",
+      cidr_blocks = [local.vpc_cidr, local.pod_cidr],
+      type        = "ingress",
+      description = "Allow all internal traffic from both the node and pod networks"
+    },
+    {
+      from_port   = 0,
+      to_port     = 0,
+      protocol    = "icmp",
+      cidr_blocks = ["0.0.0.0/0"],
+      type        = "ingress",
+      description = "Allow all icmp traffic from all sources"
+    },
+    {
+      from_port   = 0,
+      to_port     = 0,
+      protocol    = "-1",
+      cidr_blocks = ["0.0.0.0/0"],
+      type        = "egress",
+      description = "Allow all egress traffic"
+    },
+  ]
 }
